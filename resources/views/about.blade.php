f4fb04063308e164c64b8a1305748bdbbaf8e838
Ummu Ghaidah Mutmainnah <ummughaidah@gmail.com>
2024-11-20 11:18:54 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>Creator: {{$nama}}</h3>
+    <img src="img/wisuda.jpeg" alt="Ummu" width="300">
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+
+<body>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto sint saepe distinctio est. Ab vel perspiciatis ut, neque voluptatibus, temporibus voluptates explicabo repellat, deserunt ad aut aliquid? Illo, iure eligendi?</p>
+    </article>
+    
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto sint saepe distinctio est. Ab vel perspiciatis ut, neque voluptatibus, temporibus voluptates explicabo repellat, deserunt ad aut aliquid? Illo, iure eligendi?</p>
+    </article>
+</body>
+
+</html>
